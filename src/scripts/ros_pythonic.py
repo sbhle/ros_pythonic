a413c0990bf136420f90154e83f731afc7dea591
Sebastian Boehle <boehle@gmail.com>
2019-01-03 19:54:27 +0100

    replaced print with ros logging

--- src/scripts/ros_pythonic.py
+++ src/scripts/ros_pythonic.py
@@ -32,7 +32,7 @@ class Node(object):
             mod = __import__(from_part, fromlist=[imp_part])
             return getattr(mod, imp_part)
         except ImportError:
-            print('Cant import given dtype!')
+            rospy.logerr('Cant import dtype: {}'.format(import_str))
             raise
 
     def topic_sub(self, sub_topic, msg_type):
@@ -125,16 +125,15 @@ class Node(object):
         else:
             node_name_init = node_name
 
-        print('-'*80)
-        print('created node: {}'.format(node_name_init))
         rospy.init_node(node_name_init, anonymous=True)
+        rospy.loginfo('created node: {}'.format(node_name_init))
 
         # subscriptions have to be done here and not in the callback
         # b/c otherwise ROS doesn't register the subscription w/o
         # a additional call of the function (which executes the function)
         for topic, dtype, cb in self.topic_subs:
             rospy.Subscriber(topic, dtype, cb)
-            print('subscribed to: {}'.format(topic))
+            rospy.loginfo('subscribed to: {}'.format(topic))
 
         try:
             # only exec main perodically if there is something to do
